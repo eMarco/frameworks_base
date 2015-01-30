fd2272bd5e2a611dd36afd42043027242558293b
Kyle Repinski <repinski23@gmail.com>
2015-01-30 14:54:10 -0600

    Force the 'Android is upgrading...' screen to use darker colors on tuna.
    The white Material theme causes noticeable burn-in on tuna's AMOLED display.

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5782,6 +5782,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     } else if (mContext.getPackageManager().hasSystemFeature(
                             PackageManager.FEATURE_TELEVISION)) {
                         theme = com.android.internal.R.style.Theme_Leanback_Dialog_Alert;
+                    } else if (SystemProperties.get("ro.product.board").equals("tuna")) {
+                        theme = com.android.internal.R.style.Theme_Holo_Dialog_Alert;
                     } else {
                         theme = 0;
                     }
